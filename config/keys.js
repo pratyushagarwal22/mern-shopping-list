55dc04a303a37dd543e2ff12ac53d2c656fa0a53
pratyushagarwal22 <agarwalpratyush2222@gmail.com>
2020-05-25 23:51:09 +0530

    Started implementing React

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://pratyush_agarwal:delta6goingdark@customer-api-p14to.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb://pratyush_agarwal:delta6goingdark@customer-api-shard-00-00-p14to.mongodb.net:27017,customer-api-shard-00-01-p14to.mongodb.net:27017,customer-api-shard-00-02-p14to.mongodb.net:27017/test?ssl=true&replicaSet=customer-api-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 // Connect to Mongo
 mongoose
-.connect(db)
-.then(() => console.log('MongoDB Connected..'))
-.catch(err => console.log(err));
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected..'))
+    .catch(err => console.log(err));
 
 // To Use the routes/api/items.js file
 app.use('/api/items', items);
@@ -23,5 +23,3 @@ app.use('/api/items', items);
 // Setting the port number
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port : ${port}`));
-
-
